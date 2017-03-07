d0193ef0c8ecac58b63ad5056625cd9c4304dd3f
Mikhail Hushchyn <mikhail.hushchyn@cern.ch>
2017-03-07 10:29:17 +0100

    Update

--- fast_hough.py
+++ fast_hough.py
@@ -118,6 +118,7 @@ class Clusterer(object):
 
         r, phi = self.get_polar(x, y)
 
+        # Go through pairs of hits
         for first_i in range(len(r)-1):
 
             r1, phi1, layer1 = r[first_i], phi[first_i], layer[first_i]
@@ -126,15 +127,19 @@ class Clusterer(object):
 
                 r2, phi2, layer2 = r[second_i], phi[second_i], layer[second_i]
 
+                # Take hits from different layers if a phi window to speed up the method
                 if numpy.abs(layer2 - layer1) == 0 or numpy.abs(phi1 - phi2) > self.phi_window:
                     continue
 
+                # Estimate track parameters based on pair of hits
                 b, a = (numpy.cos(phi2) * r1 / r2 - numpy.cos(phi1)), (numpy.sin(phi1) - numpy.sin(phi2) * r1 / r2)
                 theta = numpy.arctan(b / a) * (a != 0) + numpy.pi * (a < 0) + 0.5 * numpy.pi * (a==0) * (b>0) + 1.5 * numpy.pi * (a==0) * (b<0)
                 invr = 2. * numpy.cos(phi1 - theta) / r1
 
+                # Estimate hits inside a bin of the track parameters space
                 track = self.hits_in_bin(r, phi, invr, theta, 0.5 * (phi1 + phi2 - self.phi_window), 0.5 * (phi1 + phi2 + self.phi_window))
 
+                # Save recognized track
                 if len(track) >= self.min_hits:
                     track_inds.append(track)
                     track_params.append([invr, theta])
@@ -190,11 +195,96 @@ class Clusterer(object):
             labels[one_track_inds] = track_id
             track_id += 1
 
-            counter +=1
-            if counter > 100:
-                break
-
         return numpy.array(labels)
+
+
+    def splitter(self, labels, X):
+        """
+        Separate two close tracks.
+
+        Parameters
+        ----------
+        labels : array-like
+            Recognized hit labels.
+        X : ndarray-like
+            Hit features.
+
+        Returns
+        ------
+        labels : array-like
+            New recognized hit labels.
+        """
+
+        x, y, layer = X[:, 2], X[:, 3], X[:, 0]
+        r, phi = self.get_polar(x, y)
+
+        ind = numpy.arange(len(X))
+        unique_labels = numpy.unique(labels[labels != -1])
+        track_id = unique_labels[-1] + 1
+        #print unique_labels
+
+        for lab in unique_labels:
+
+            track_ind = ind[labels == lab]
+
+            track_layer = layer[track_ind]
+            track_phi = phi[track_ind]
+
+            track1 = []
+            track2 = []
+
+            for l in numpy.unique(track_layer):
+
+                ind_layer = track_ind[track_layer == l]
+                phi_layer = track_phi[track_layer == l]
+
+                hit_loc_ind = numpy.argsort(phi_layer)
+
+                track1.append(ind_layer[hit_loc_ind[0]])
+
+                if len(ind_layer) > 1:
+
+                    track2.append(ind_layer[hit_loc_ind[-1]])
+
+            if len(track2)>=2:
+                labels[track2] = track_id
+                track_id += 1
+
+        return labels
+
+    def marker(self, labels, X):
+        """
+        Marks unlabeled hits.
+
+        Parameters
+        ----------
+        labels : array-like
+            Recognized hit labels.
+        X : ndarray-like
+            Hit features.
+
+        Returns
+        ------
+        labels : array-like
+            New recognized hit labels.
+        """
+
+        x, y, layer = X[:, 2], X[:, 3], X[:, 0]
+        r, phi = self.get_polar(x, y)
+
+        ind = numpy.arange(len(X))
+
+        unlabeled_hits = ind[(labels == -1)*(layer == 0)]
+        l1_hits = ind[(layer == 1)]
+
+        for hit_ind in unlabeled_hits:
+
+            dist = (x[hit_ind] - x[l1_hits])**2 + (y[hit_ind] - y[l1_hits])**2
+            nearest_hit_ind = l1_hits[dist == dist.min()][0]
+
+            labels[hit_ind] = labels[nearest_hit_ind]
+
+        return labels
 
     def fit(self, X, y):
         pass
@@ -220,11 +310,11 @@ class Clusterer(object):
         self.track_inds_ = track_inds
         self.track_params_ = track_params
 
-
+        # Assign one track label to hits
         labels = self.get_hit_labels(track_inds, len(X))
 
+        # Additional processing of the recognized labels (+1-2% to score)
+        labels = self.splitter(labels, X)
+        labels = self.marker(labels, X)
+
         return labels
-
-
-
-
